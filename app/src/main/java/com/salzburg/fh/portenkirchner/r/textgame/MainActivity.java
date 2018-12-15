ec79cd77775c74334903c55a0b51a35c8d8f5ce2
Clemens Johannes Zuzan <czuzan.its-b2015@fh-salzburg.ac.at>
2018-12-15 14:08:03 +0100

    second commit

--- app/src/main/java/com/salzburg/fh/portenkirchner/r/textgame/MainActivity.java
+++ app/src/main/java/com/salzburg/fh/portenkirchner/r/textgame/MainActivity.java
@@ -1,15 +1,33 @@
 package com.salzburg.fh.portenkirchner.r.textgame;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    Button btnTextauswahl;
+
+    String filename = "Pflaume.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button)findViewById(R.id.btn_start);
+        btnTextauswahl = (Button)findViewById(R.id.btn_textauswahl);
+
     }
 
-    onClick_btn_start()
+    public void onClick_btn_start(View v){
+        Intent i = new Intent(this, TextActivity.class);
+        i.putExtra("filename", filename);
+        startActivity(i);
+    }
 }
--- app/src/main/java/com/salzburg/fh/portenkirchner/r/textgame/TextActivity.java
+++ app/src/main/java/com/salzburg/fh/portenkirchner/r/textgame/TextActivity.java
@@ -2,25 +2,81 @@ package com.salzburg.fh.portenkirchner.r.textgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class TextActivity extends AppCompatActivity {
 
-    Button btnStart;
-    Button btnTextauswahl;
+
+    TextView tvFilename;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String filename = "";
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
 
-        btnStart = (Button)findViewById(R.id.btn_start);
-        btnTextauswahl = (Button)findViewById(R.id.btn_textauswahl);
+        tvFilename = findViewById(R.id.tv_filename);
+        tvLueckentext = findViewById(R.id.tv_lueckentext);
+
+        Bundle extras = getIntent().getExtras();
+        if (!extras.isEmpty()) {
+            filename = extras.getString("filename");
+            tvFilename.setText(filename);
+        }
+
+        char current;
+        byte[] buffer;
+        String[][] lueckenArray = {};
+        try {
+            InputStream input = getAssets().open(filename);
+            int size = input.available();
+            buffer = new byte[size];
+
+            int bufferZaehler = 0;
+            int lsgZaehler = 0;
+            int lueckenZaehler = 0;
+            boolean altFlag = false;
+
+            while (input.available() > 0) {
+                current = (char) input.read();
+                Log.d("caracter:", "" + current);
+                if (current == '<') {
+                    altFlag = true;
+                    lueckenArray[lueckenZaehler][lsgZaehler] = (String) "";
+                    continue;
+                }
+                if (current == '>') {
+                    lueckenZaehler++;
+                    altFlag = false;
+                    lsgZaehler = 0;
+                    continue;
+                }
+                if (current == ',') {
+                    lsgZaehler++;
+                    lueckenArray[lueckenZaehler][lsgZaehler] = (String) "";
+                    continue;
+                }
+                if (altFlag == true) {
+                    lueckenArray[lueckenZaehler][lsgZaehler] += current;
+                    buffer[bufferZaehler] = (char) '_';
+                    bufferZaehler++;
+                } else {
+                    buffer[bufferZaehler] = (char) current;
+                    bufferZaehler++;
+                }
+            }
+
+        }
+        catch(IOException ex){
+                Log.d("ERROR DETECTED", "ERROR WHILE TRYING TO OPEN FILE");
+        }
     }
-
-    public void onClick_btn_start()
-    {
-    }
-
-
 }
